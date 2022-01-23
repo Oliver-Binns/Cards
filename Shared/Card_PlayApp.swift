aa4bad15adf7ed0cec224f62a7998402fac8b037
Oliver BINNS <mail@oliverbinns.co.uk>
2022-01-23 10:37:04 +0000

    Initial Commit

---
+++ Shared/Card_PlayApp.swift
@@ -0,0 +1,17 @@
+//
+//  Card_PlayApp.swift
+//  Shared
+//
+//  Created by Binns, Oliver on 23/01/2022.
+//
+
+import SwiftUI
+
+@main
+struct Card_PlayApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
