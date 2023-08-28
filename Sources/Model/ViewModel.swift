b350bcaff52b0d6d0818946b67fe91759f51812d
Oliver BINNS <mail@oliverbinns.co.uk>
2023-08-28 16:39:42 +0100

    fix: added logic for late joiners

--- Sources/Model/ViewModel.swift
+++ Sources/Model/ViewModel.swift
@@ -1,12 +1,15 @@
 import CardsModel
 import Combine
-import GroupActivities
 import Sevens
 import SwiftUI
 
 final class ViewModel: ObservableObject {
     @Published var error: Error?
-    @Published var game: (any Game)?
+    @Published var game: (any Game)? {
+        didSet {
+            sharePlay.startGame()
+        }
+    }
     
     private var tasks: [Task<(), Never>] = []
     @Published private(set) var players: [NonLocalPlayer] = [] {
@@ -34,6 +37,8 @@ final class ViewModel: ObservableObject {
     private var cancellables: Set<AnyCancellable> = []
     
     init() {
+        sharePlay.delegate = self
+        
         sharePlay.$session
             .receive(on: RunLoop.main)
             .map(\.?.activity.game)
@@ -59,77 +64,34 @@ final class ViewModel: ObservableObject {
     }
     
     func playCard(_ card: PlayingCard?) {
-        game?.play(card: card)
-        
         Task {
-            do {
-                try await sharePlay.sendMove(card)
-            } catch {
-                self.error = error
-            }
+            try await playMove(card, locally: true)
         }
     }
     
     func followPlayer(_ player: NonLocalPlayer) async {
         for await move in player.moves {
             try? await Task.sleep(for: .seconds(0.7))
+            try? await playMove(move, locally: false)
+        }
+    }
+    
+    func playMove(_ move: PlayingCard?, locally: Bool) async throws {
+        do {
+            try await sharePlay.didPlayMove(move, locally: locally)
             await MainActor.run {
                 withAnimation {
-                    self.game?.play(card: move)
+                    game?.play(card: move)
                 }
             }
+        } catch {
+            self.error = error
         }
     }
 }
 
-final class SharePlayModel: ObservableObject {
-    private let groupStateObserver = GroupStateObserver()
-    
-    @Published private(set) var session: GroupSession<PlayTogether>? = nil
-    private var messenger: GroupSessionMessenger? = nil
-    
-    /// A SharePlay connection exists:
-    /// this is either an active FaceTime call or iMessage SharePlay session
-    ///
-    /// if true, we can directly activate a shared activity
-    /// if false, we must state a connection using `GroupActivitySharingController`
-    var activeConnectionExists: Bool {
-        groupStateObserver.isEligibleForGroupSession
-    }
-    
-    /// Whether `GroupActivitySharingController` is available on the current platform
-    ///
-    /// If this API is available, then we can allow the user to open a new SharePlay session within the app
-    var canOpenConnection: Bool {
-        guard #available(iOS 15.4, macOS 13, *) else {
-            return false
-        }
-        #if os(macOS) && swift(<5.9)
-        return false
-        #else
-        return true
-        #endif
-    }
-    
-    init() {
-        Task {
-            await findSessions()
-        }
-    }
-    
-    func findSessions() async {
-        for await session in PlayTogether.sessions() {
-            await MainActor.run {
-                // todo: when should activity be joined?
-                session.join()
-                
-                self.session = session
-                self.messenger = GroupSessionMessenger(session: session)
-            }
-        }
-    }
-    
-    func sendMove(_ card: PlayingCard?) async throws {
-        try await messenger?.send(card)
+extension ViewModel: SharePlayModelDelegate {
+    func resumeGame(moves: [CardsModel.PlayingCard?]) {
+        moves.forEach { game?.play(card: $0) }
     }
 }
